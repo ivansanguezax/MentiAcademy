4684aafd69f93a092b1316dca8608c253c6cacb9
ivansanguezax <ivansanguezax@gmail.com>
2023-01-31 17:01:11 -0400

    slider accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,46 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor:true,
+    centeredSlides: true,
+    slidesPreView: 'auto',
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item');
+accordionItems.forEach((item)=>{
+    const accordionHeader = item.querySelector('.value__accordion-header');
+
+    accordionHeader.addEventListener('click', ()=>{
+        const openItem = document.querySelector('.accordion-open')
+        toogleItem(item)
+
+        if(openItem && openItem!== item){
+            toogleItem(openItem)
+        }
+    })
+});
+
+const toogleItem = (item)=>{
+    const accordionContent = item.querySelector('.value__accordion-content');
+    
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px' 
+        item.classList.add('accordion-open');
+    }
+}
+
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
